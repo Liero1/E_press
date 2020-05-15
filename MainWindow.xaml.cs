eb1c38952b5149aa4ed7d02ae8aa6f98d36f1076
Liero1 <59914594+Liero1@users.noreply.github.com>
2020-05-15 14:46:57 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Wydawnictwo_ePress
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Open Main Page
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainPage mainpage = new MainPage();
+            Main.NavigationService.Navigate(mainpage);      
+        }
+        
+    }
+}
---
+++ PrintingHouseWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace E_press
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PrintingHouseWindow.xaml
+    /// </summary>
+    public partial class PrintingHouseWindow : Window
+    {
+        public PrintingHouseWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
